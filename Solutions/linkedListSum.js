01664200a9d57fabbb5f4648f3b7a3d45a0bc4fd
Krista Loven <krista.loven.87@gmail.com>
2020-01-23 12:12:16 -0800

    Updated Linked List Sum Soluntion

--- Solutions/linkedListSum.js
+++ Solutions/linkedListSum.js
@@ -3,23 +3,29 @@
 // such that the 1's digit is at the head of the list. Write a function
 // that adds the two numbers and returns the sum as a linked list.
 
-const = linkedListSum = (LinkedList1, LinkedList2) => {
-  let digit = new LinkedListNode()
-  let summed = new LinkedList(digit)
+const linkedListSum = (LinkedList1, LinkedList2) => {
+  const head = new ListNode(0)
+  let digit = head
   let overflow = 0
-  let marker1 = LinkedList1.head
-  let marker2 = LinkedList2.head
-  let sum = marker1.data + marker2.data + overflow
+  let marker1 = LinkedList1
+  let marker2 = LinkedList2
 
-  while (sum > 0) {
-    sum < 10 ? overflow = 0 : overflow = 1
-
-    digit.data = sum % 10
-    digit.next = new LinkedListNode()
+  while (marker1 || marker2 || overflow > 0) {
+    digit.next = new ListNode()
     digit = digit.next
-    marker1 = marker1.next
-    marker2 = marker2.next
+    let total = overflow
+    
+    if (marker1) { 
+      total += marker1.val 
+      marker1 = marker1.next
+    }
+    if (marker2) { 
+      total += marker2.val 
+      marker2 = marker2.next
+    }
+    
+    digit.val = total % 10
+    overflow = total < 10 ? 0 : 1
   }
-
-  return summed
+  return head.next ? head.next : head
 }
